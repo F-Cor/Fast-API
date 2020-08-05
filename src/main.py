9fb3f10e4364fa54edb9eea480599081a1e074d3
Nckflannery <Nckflannery@users.noreply.github.com>
2020-08-05 15:27:07 -0700

    Post again

--- src/main.py
+++ src/main.py
@@ -4,22 +4,21 @@ from fastapi import Request
 
 
 app = FastAPI()
-
-# PASSWORD = ''
+PASSWORD = ''
 
 @app.get('/')
 async def root(request: Request):
     return {'message': "Welcome to the Spelling Bee FastAPI"}
 
-# @app.get('/password/{password}')
-# async def get_new_pass(password: str):
-#     global PASSWORD 
-#     PASSWORD = password
-#     return f'Password updated'
+@app.post('/password')
+async def get_new_pass(password: str):
+    global PASSWORD 
+    PASSWORD = password
+    return f'Password updated'
 
-@app.get('/db_update')#/{password}')
-async def db_update():#password: str):
-    # global PASSWORD
-    # if password == PASSWORD:    
+@app.post('/db_update')
+async def db_update(password: str):
+    global PASSWORD
+    if password == PASSWORD:    
         c = Word_Items()
         return(c.update_db())
