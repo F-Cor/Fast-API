9176e7da2da40b180b1c797bf1b25c1bdc82fbed
Nckflannery <Nckflannery@users.noreply.github.com>
2020-09-03 12:16:40 -0700

    Adjust job time for deleting old database

--- jobs.py
+++ jobs.py
@@ -6,6 +6,9 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron', day_of_week='0-6', hour='0, 12', timezone='UTC')
 def update_database():
     db_updater()
+    
+@sched.scheduled_job('cron', day_of_week='0-6', hour='0, 12', minute='2', timezone='UTC')
+def delete_old_table():
     del_table()
 
 sched.start()
